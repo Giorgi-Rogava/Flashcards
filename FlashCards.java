3663fa2d914fb61debb2dcebfa3b64843983f19b
Giorgi-Rogava <giorgi.rogava.1@btu.edu.ge>
2019-08-28 22:31:03 +0400

    Implemented Stage #2

---
+++ FlashCards.java
@@ -0,0 +1,22 @@
+package flashcards;
+
+import java.util.*;
+
+public class FlashCards {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+
+        String card = scanner.nextLine();
+        String definition = scanner.nextLine();
+        String answer = scanner.nextLine();
+
+        if (answer.equals(definition)) {
+            System.out.println("Your answer is right!");
+        } else {
+            System.out.println("Your answer is wrong...");
+        }
+
+    }
+}
